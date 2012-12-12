474775140191c82079bbdf70a76848044efa0ab6
Logan Chien <tzuhsiang.chien@gmail.com>
2012-12-12 23:43:15 +0800

    Make Tile.hpp compilable.

--- Tile.cpp
+++ Tile.cpp
@@ -1,1 +1,16 @@
 #include "Tile.hpp"
+
+#include <stdlib.h>
+
+bool Tile::IsBoundaryTile()
+{
+#if 0
+    int2 blockSize = coreBlock->processor->coreBlockSize;
+    int2 coreOrigin = coreBlock->ComputeCoreBlockOrigin();
+    return tileIdx.x == coreOrigin.x && tileIdx.y == coreOrigin.y &&
+        tileIdx.x == coreOrigin.x + blockSize.x - 1 && tileIdx.y == coreOrigin.y + blockSize.y - 1;
+#else
+    assert(0 && "not implemented");
+    abort();
+#endif
+}
--- Tile.hpp
+++ Tile.hpp
@@ -1,45 +1,50 @@
+#ifndef TILE_HPP
+#define TILE_HPP
 
+#include "simutil.hpp"
+
+struct Core;
+struct CoreBlock;
+struct MMU;
+struct Router;
 
 /// A tile consists of a core and a router
 struct Tile
 {
     /// Index of this tile within the core block
     int2 tileIdx;
-    
+
     /// The block to which this tile belongs
     CoreBlock* coreBlock;
-    
+
     /// The core of this tile (ARMUlator class)
     Core* core;
-    
+
     /// The MMU of the core (modified ARMUlator class)
     MMU* mmu;
-    
+
     /// The router of this tile
-    Router router;
-    
+    Router* router;
+
     /// Amount of normalized ticks passed since initialization
     long long coreTime;
-    
+
     /// Whether this tile is not currently doing anything
     bool tileIdle;
-    
-    
-    void InitTile(const Index& tileIdx, CoreBlock* coreBlock)
+
+
+    void InitTile(const int2& tileIdx, CoreBlock* coreBlock)
     {
         // TODO: Init index, Core etc.
-        
+#if 0
+        // FIXME: Not implemented
         core = new Core(params);
         mmu = core->mmu;
+#endif
     }
-
 
     /// Whether this tile is at the core's x = 0, y = 0, x = w-1 or y = h-1
-    bool IsBoundaryTile()
-    {
-        int2 blockSize = coreBlock->processor->coreBlockSize;
-        int2 coreOrigin = coreBlock->ComputeCoreBlockOrigin();
-        return tileIdx.x == coreOrigin.x && tileIdx.y == coreOrigin.y &&
-            tileIdx.x == coreOrigin.x + blockSize.x - 1 && tileIdx.y == coreOrigin.y + blockSize.y - 1;
-    }
+    bool IsBoundaryTile();
 };
+
+#endif // TILE_HPP
