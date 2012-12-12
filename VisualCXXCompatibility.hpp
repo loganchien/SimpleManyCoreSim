e26319d14e0fd82e6fcff06fcd575482b3801d3a
Logan Chien <tzuhsiang.chien@gmail.com>
2012-12-12 23:17:41 +0800

    Add Visual C++ compatibility layer for GCC.

---
+++ VisualCXXCompatibility.hpp
@@ -0,0 +1,13 @@
+#ifndef VISUAL_CXX_COMPATIBILITY_HPP
+#define VISUAL_CXX_COMPATIBILITY_HPP
+
+#ifndef _MSC_VER
+
+#include <utility>
+#include <stdint.h>
+
+typedef uint16_t WORD;
+
+#endif
+
+#endif // VISUAL_CXX_COMPATIBILITY_HPP
