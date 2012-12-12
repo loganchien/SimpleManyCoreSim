348399356e028789a6d037afef72e7cd4325db20
Logan Chien <tzuhsiang.chien@gmail.com>
2012-12-12 23:51:49 +0800

    Make CoreBlock.hpp compilable.

--- CoreBlock.cpp
+++ CoreBlock.cpp
@@ -1,1 +1,63 @@
 #include "CoreBlock.hpp"
+
+// FIXME: Fix Tile.hpp, Processor.hpp, and Thread.hpp first.
+#if 0
+#include "Tile.hpp"
+#include "SimConfig.hpp"
+#include "Processor.hpp"
+#include "Thread.hpp"
+
+CoreBlock::CoreBlock()
+{
+    tiles = new Tile[GlobalConfig.CoreBlockSize()];
+}
+
+CoreBlock::~CoreBlock()
+{
+    delete [] tiles;
+}
+
+int2 CoreBlock::ComputeCoreBlockOrigin() const
+{
+    return blockIdx * processor->coreBlockSize;
+}
+
+void CoreBlock::ScheduleThread(TaskBlock& taskBlock, Tile& tile)
+{
+    Thread nextThread = taskBlock.CreateNextThread(tile);
+
+#ifdef _VERBOSE
+    if (nextThread.threadIdx.Area() % 100)
+    {
+        PrintLine("Thread starting: " << task.name << " (" << nextThread.threadIdx.x << ", " << nextThread.threadIdx.y << ") on Tile (" <<
+                  << tile.tileIdx.x << ", " << tile.tileIdx.y << ")");
+    }
+#endif
+    tile.core.StartThread(nextThread);
+}
+
+void CoreBlock::OnThreadFinished(Thread& thread)
+{
+#ifdef _VERBOSE
+    if (nextThread.threadIdx.Area() % 100)
+    {
+        PrintLine("Thread fininshed: " << task.name << " (" << nextThread.threadIdx.x << ", " << nextThread.threadIdx.y << ")");
+    }
+#endif
+    if (thread.taskBlock->HasMoreThreads())
+    {
+        // Schedule next thread
+        ScheduleThread(*thread.taskBlock, *thread.tile);
+    }
+    else if (thread.taskBlock->IsFinished())
+    {
+        // All blocks of this task have already been scheduled
+        processor->OnTaskBlockFinished(*thread.taskBlock);
+    }
+    else
+    {
+        // Do nothing: The tile is now idle
+        PrintLine("Tile idle: " << " (" << thread.tile->tileIdx.x << ", " << thread.tile->tileIdx.y << ")");
+    }
+}
+#endif
--- CoreBlock.hpp
+++ CoreBlock.hpp
@@ -1,3 +1,14 @@
+#ifndef CORE_BLOCK_HPP
+#define CORE_BLOCK_HPP
+
+#include "SimConfig.hpp"
+#include "simutil.hpp"
+
+struct Processor;
+struct TaskBlock;
+struct Tile;
+struct Thread;
+
 /// A core block contains multiple tiles that are used to schedule the threads of one TaskBlock
 struct CoreBlock
 {
@@ -6,38 +17,28 @@ struct CoreBlock
 
     /// All tiles in this block
     Tile* tiles;
-    
+
     /// The index of this core block inside the grid
     int2 blockIdx;
-    
-    CoreBlock()
-    {
-        tiles = new Tile[GlobalConfig.CoreBlockSize()];
-    }
 
-    ~CoreBlock()
-    {
-        delete [] tiles;
-    }
-    
+    CoreBlock();
+
+    ~CoreBlock();
 
     /// Initializes this CoreBlock
-    void InitCoreBlock(params)
+    void InitCoreBlock()
     {
+#if 0
         foreach (tile in tiles)
         {
             // TODO: Pass init parameters to each tile
             tile.InitTile(...);
         }
+#endif
     }
-    
 
     /// The index of the first tile within this core block
-    int2 ComputeCoreBlockOrigin() const
-    {
-        return blockIdx * processor->coreBlockSize;
-    }
-
+    int2 ComputeCoreBlockOrigin() const;
 
     /// Computes the tile index of the given L2 chunk within this core block
     int2 ComputeTileIndex(int l2ChunkIdx) const
@@ -45,13 +46,11 @@ struct CoreBlock
         return ComputeCoreBlockOrigin() + GlobalConfig.ComputeInCoreBlockIdx2(l2ChunkIdx);
     }
 
-    
     /// Do Z-order lookup (simple conversion from cache entry index to tile)
     int ComputeL2ChunkIndex(const Address& addr) const
     {
         return ComputeL2ChunkIndex(addr.GetL2ChunkIndex());
     }
-
 
     /// Computes the L2 chunk index of the given tile index
     int ComputeL2TileChunkIndex(int2 tileIdx) const
@@ -60,53 +59,17 @@ struct CoreBlock
         return ComputeL2ChunkIndex(tileIdx - blockOrigin);
     }
 
-
     /// Computes the L2 chunk index of the given in-block indexs
     int ComputeL2ChunkIndex(int2 chunkIdx2) const
     {
         // TODO: Domi
     }
 
-
     /// Put the next thread of the given TaskBlock on the given tile
-    void ScheduleThread(TaskBlock& taskBlock, Tile& tile)
-    {   
-        Thread nextThread = taskBlock.CreateNextThread(tile);
-
-#ifdef _VERBOSE
-        if (nextThread.threadIdx.Area() % 100)
-        {
-            PrintLine("Thread starting: " << task.name << " (" << nextThread.threadIdx.x << ", " << nextThread.threadIdx.y << ") on Tile (" << 
-                << tile.tileIdx.x << ", " << tile.tileIdx.y << ")";
-        }
-#endif
-        tile.core.StartThread(nextThread);
-    }
-
+    void ScheduleThread(TaskBlock& taskBlock, Tile& tile);
 
     /// Is called by Core when it reached the end of it's current instruction stream
-    void OnThreadFinished(Thread& thread)
-    {
-#ifdef _VERBOSE
-        if (nextThread.threadIdx.Area() % 100)
-        {
-            PrintLine("Thread fininshed: " << task.name << " (" << nextThread.threadIdx.x << ", " << nextThread.threadIdx.y << ")");
-        }
-#endif
-        if (thread.taskBlock->HasMoreThreads())
-        {
-            // Schedule next thread
-            ScheduleThread(*thread.taskBlock, *thread.tile);
-        }
-        else if (thread.taskBlock->IsFinished())
-        {
-            // All blocks of this task have already been scheduled
-            processor->OnTaskBlockFinished(*thread.taskBlock);
-        }
-        else
-        {
-            // Do nothing: The tile is now idle
-            PrintLine("Tile idle: " << " (" << thread.tile->tileIdx.x << ", " << thread.tile->tileIdx.y << ")");
-        }
-    }
+    void OnThreadFinished(Thread& thread);
 };
+
+#endif // CORE_BLOCK_HPP
