8edacaebcf9c2ab2df70ccc80be7d4c91ec6d046
Logan Chien <tzuhsiang.chien@gmail.com>
2013-01-03 00:03:12 +0800

    Launch debugger when debug option is given.

--- tools/run_all.sh
+++ tools/run_all.sh
@@ -9,7 +9,12 @@ BENCHMARK_DIR="${ROOT}/benchmark"
 
 TASKS="${BENCHMARK_DIR}/matrix_simple.ini"
 
+
 for cfg in ${SIMCONFIG_DIR}/*; do
   echo "### Running ${cfg} ..."
-  ${BUILD_DIR}/TileSim "${cfg}" ${TASKS}
+  if [ "$1" = "debug" ]; then
+    gdb --args ${BUILD_DIR}/TileSim "${cfg}" ${TASKS}
+  else
+    ${BUILD_DIR}/TileSim "${cfg}" ${TASKS}
+  fi
 done
