c1e9e7f137e257c625be6306140cf381c8212b34
Logan Chien <tzuhsiang.chien@gmail.com>
2013-01-04 09:23:23 +0800

    Move the code to TileMMU.cpp.

---
+++ external/armulator/src/TileMMU.cpp
@@ -0,0 +1,85 @@
+#include "TileMMU.h"
+
+MMU::MMU(const char *file_name)
+{
+}
+
+MMU::~MMU()
+{
+}
+
+//! Give out the Thumb instruction
+T_INSTR MMU::getInstr(int address)
+{
+    return 0;
+}
+
+//! Give out the ARM instruction
+T_INSTR MMU::getInstr32(int address)
+{
+    return 0;
+}
+
+//! Output byte data by address
+BYTE MMU::get_byte(int address)
+{
+    return 0;
+}
+
+//! Output halfword data by address
+HALFWORD MMU::get_halfword(int address)
+{
+    return 0;
+}
+
+//! Output word data by address
+WORD MMU::get_word(int address)
+{
+    return 0;
+}
+
+//! Input byte data by address
+void MMU::set_byte(int address, BYTE data)
+{
+}
+
+//! Input halfword data by address
+void MMU::set_halfword(int address, HALFWORD data)
+{
+}
+
+//! Input word data by address
+void MMU::set_word(int address, WORD data)
+{
+}
+
+
+//! Give out the starting PC
+int MMU::getEntry()
+{
+    return 0;
+}
+
+//! Give out the high address of stack
+int MMU::getStackTop()
+{
+    return 0;
+}
+
+//! Give out the size of stack
+int MMU::getStackSz()
+{
+    return 0;
+}
+
+//! Give out the address of heap
+int MMU::getHeapTop()
+{
+    return 0;
+}
+
+//! Give out the size of heap
+int MMU::getHeapSz()
+{
+    return 0;
+}
--- external/armulator/src/TileMMU.h
+++ external/armulator/src/TileMMU.h
@@ -6,89 +6,48 @@
 class MMU
 {
 public:
-    MMU(const char *file_name)
-    {
-    }
+    MMU(const char *file_name);
 
-    ~MMU()
-    {
-    }
+    ~MMU();
 
     //! Give out the Thumb instruction
-    T_INSTR getInstr(int address)
-    {
-        return 0;
-    }
+    T_INSTR getInstr(int address);
 
     //! Give out the ARM instruction
-    T_INSTR getInstr32(int address)
-    {
-        return 0;
-    }
+    T_INSTR getInstr32(int address);
 
     //! Output byte data by address
-    BYTE get_byte(int address)
-    {
-        return 0;
-    }
+    BYTE get_byte(int address);
 
     //! Output halfword data by address
-    HALFWORD get_halfword(int address)
-    {
-        return 0;
-    }
+    HALFWORD get_halfword(int address);
 
     //! Output word data by address
-    WORD get_word(int address)
-    {
-        return 0;
-    }
+    WORD get_word(int address);
 
     //! Input byte data by address
-    void set_byte(int address, BYTE data)
-    {
-    }
+    void set_byte(int address, BYTE data);
 
     //! Input halfword data by address
-    void set_halfword(int address, HALFWORD data)
-    {
-    }
+    void set_halfword(int address, HALFWORD data);
 
     //! Input word data by address
-    void set_word(int address, WORD data)
-    {
-    }
-
+    void set_word(int address, WORD data);
 
     //! Give out the starting PC
-    int getEntry()
-    {
-        return 0;
-    }
+    int getEntry();
 
     //! Give out the high address of stack
-    int getStackTop()
-    {
-        return 0;
-    }
+    int getStackTop();
 
     //! Give out the size of stack
-    int getStackSz()
-    {
-        return 0;
-    }
+    int getStackSz();
 
     //! Give out the address of heap
-    int getHeapTop()
-    {
-        return 0;
-    }
+    int getHeapTop();
 
     //! Give out the size of heap
-    int getHeapSz()
-    {
-        return 0;
-    }
+    int getHeapSz();
 };
 
 #endif // __TILE_MMU__
