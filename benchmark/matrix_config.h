ca34359c7d190e13075f32840b099f437ef7f085
Logan Chien <tzuhsiang.chien@gmail.com>
2013-01-13 19:58:18 +0800

    Add more input matrix data set.

--- benchmark/matrix_config.h
+++ benchmark/matrix_config.h
@@ -8,9 +8,21 @@
 #if SIZE == 16
 #define MATRIX_A_INPUT_FILE "inputs/matrix16_A.txt"
 #define MATRIX_B_INPUT_FILE "inputs/matrix16_B.txt"
+#elif SIZE == 32
+#define MATRIX_A_INPUT_FILE "inputs/matrix32_A.txt"
+#define MATRIX_B_INPUT_FILE "inputs/matrix32_B.txt"
+#elif SIZE == 64
+#define MATRIX_A_INPUT_FILE "inputs/matrix64_A.txt"
+#define MATRIX_B_INPUT_FILE "inputs/matrix64_B.txt"
+#elif SIZE == 128
+#define MATRIX_A_INPUT_FILE "inputs/matrix128_A.txt"
+#define MATRIX_B_INPUT_FILE "inputs/matrix128_B.txt"
 #elif SIZE == 256
 #define MATRIX_A_INPUT_FILE "inputs/matrix256_A.txt"
 #define MATRIX_B_INPUT_FILE "inputs/matrix256_B.txt"
+#elif SIZE == 512
+#define MATRIX_A_INPUT_FILE "inputs/matrix512_A.txt"
+#define MATRIX_B_INPUT_FILE "inputs/matrix512_B.txt"
 #endif
 
 #endif /* MATRIX_CONFIG_H */
